13dd291dc068ff89e151addf38f11b7cee6463b1
Docvel <docvel@yandex.ru>
2023-03-11 01:16:38 +0300

    task 32 is solved, tests passed successfully

--- task32.py
+++ task32.py
@@ -8,6 +8,7 @@
 # Вывод: [1, 9, 13, 14, 19]
 
 # pytest -v tests\test_32.py
+from addInterface import correct_int, creat_int_list
 
 
 def is_in_mass(num_lst: list[int],
@@ -15,4 +16,19 @@ def is_in_mass(num_lst: list[int],
                max_num: int) -> list[int]:
     """Возвращает список индексов элементов списка, которые
     находятся в диапазоне [min_num,max_num] """
-    pass
+
+    output_list = []
+    for i in range(len(num_lst)):
+        if num_lst[i] >= min_num and num_lst[i] <= max_num:
+            output_list.append(i)
+    return output_list
+
+
+input_lst = creat_int_list(51, 1, 50)
+print("Введите начало диапазона: ", end="")
+minimum = correct_int()
+print("Введите конец диапазона: ", end="")
+maximum = correct_int()
+output_lst = is_in_mass(input_lst, minimum, maximum)
+print(f"Входной список: {input_lst}.\nДиапазон от {minimum} "
+      f"до {maximum}.\nИндексы элементов: {output_lst}.")
